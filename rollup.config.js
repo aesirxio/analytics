f16e647cec6e0c640f2653cd95b581df081b3a23
Viet Nguyen <viet.nguyen@redweb.dk>
2022-11-04 17:52:26 +0700

    Rollup build bundles

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,46 @@
 import 'dotenv/config';
-import buble from '@rollup/plugin-buble';
-import replace from '@rollup/plugin-replace';
 import { terser } from 'rollup-plugin-terser';
+import { babel } from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { babel } from '@rollup/plugin-babel';
 import json from '@rollup/plugin-json';
+import buble from '@rollup/plugin-buble';
+import replace from '@rollup/plugin-replace';
 import { uglify } from 'rollup-plugin-uglify';
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'build/analytics.js',
-    format: 'iife',
-    name: 'analytics',
-  },
+  output: [
+    {
+      file: 'build/analytics.js',
+      format: 'iife',
+      name: 'AesirAnalytics',
+    },
+    {
+      file: 'build/lib/bundles/bundle.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+    {
+      file: 'build/lib/bundles/bundle.esm.min.js',
+      format: 'esm',
+      plugins: [terser()],
+      sourcemap: true,
+    },
+    {
+      file: 'build/lib/bundles/bundle.umd.js',
+      format: 'umd',
+      name: 'AesirAnalytics',
+      sourcemap: true,
+    },
+    {
+      file: 'build/lib/bundles/bundle.umd.min.js',
+      format: 'umd',
+      name: 'AesirAnalytics',
+      plugins: [terser()],
+      sourcemap: true,
+    },
+  ],
   plugins: [
     nodeResolve({
       jsnext: true,
