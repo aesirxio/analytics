9c950a86a71c77f286b6562cde2f3e23fddc5de2
Viet Nguyen <viet.nguyen@redweb.dk>
2022-11-14 14:16:18 +0700

    Optimize code

--- src/analytics.js
+++ src/analytics.js
@@ -1,2 +1,113 @@
-import { AesirAnalytics } from './utils';
+import { endTracker, initTracker, startTracker } from './utils';
+
+const AesirAnalytics = () => {
+  const hook = (_this, method, callback) => {
+    const orig = _this[method];
+
+    return (...args) => {
+      callback.apply(null, args);
+
+      return orig.apply(_this, args);
+    };
+  };
+
+  /* Handle history changes */
+
+  const handlePush = async (url) => {
+    if (!url) return;
+    const { pathname, search, origin } = location;
+    url = `${origin}${pathname}${search}`;
+    const newUrl = url.toString();
+
+    if (newUrl.substring(0, 4) === 'http') {
+      url = '/' + newUrl.split('/').splice(3).join('/');
+    } else {
+      url = newUrl;
+    }
+
+    // if (currentUrl !== currentRef) {
+    //   await initTracker();
+    //   startTracker();
+    // }
+  };
+
+  /* Global */
+
+  if (!window.tracker) {
+    const tracker = (eventValue) => eventValue;
+    tracker.initTracker = initTracker;
+    tracker.startTracker = startTracker;
+
+    window.tracker = tracker;
+  }
+
+  /* Start */
+
+  history.pushState = hook(history, 'pushState', handlePush);
+  history.replaceState = hook(history, 'replaceState', handlePush);
+
+  const envParams = process.env;
+  const hostUrl = envParams.ENDPOINT_ANALYTICS_URL ? envParams.ENDPOINT_ANALYTICS_URL : '';
+
+  const root = hostUrl ? hostUrl.replace(/\/$/, '') : '';
+
+  const update = async () => {
+    if (document.readyState === 'complete') {
+      const responseInit = await initTracker(root);
+      if (responseInit) {
+        responseInit.result.event_id && insertParam('event_id', responseInit.result.event_id);
+        insertParam('uuid', responseInit.result.uuid);
+        replaceUrl();
+      }
+      const responseStart = await startTracker(root);
+      if (responseStart) {
+        responseStart.result.event_id &&
+          insertParam('event_id_start', responseStart.result.event_id);
+        insertParam('uuid_start', responseStart.result.uuid);
+        replaceUrl();
+      }
+    }
+  };
+
+  document.addEventListener('readystatechange', update, true);
+
+  window.addEventListener('beforeunload', async () => {
+    await endTracker(root);
+  });
+
+  update();
+};
+const insertParam = (key, value) => {
+  const url = new URL(window.location.href);
+  url.searchParams.set(key, value);
+  window.history.pushState({ path: url.href }, '', url.href);
+};
+
+const replaceUrl = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const event_id = urlParams.get('event_id');
+  const uuid = urlParams.get('uuid');
+
+  let anchors = document.getElementsByTagName('a');
+  for (let i = 0; i < anchors.length; i++) {
+    const eventIdParams = getParameterByName('event_id', anchors[i].href);
+    const uuidParams = getParameterByName('uuid', anchors[i].href);
+    if (anchors[i].href) {
+      const url = new URL(anchors[i].href);
+      !eventIdParams && event_id && url.searchParams.append('event_id', event_id);
+      !uuidParams && uuid && url.searchParams.append('uuid', uuid);
+      anchors[i].href = url.href;
+    }
+  }
+};
+
+const getParameterByName = (name, url = window.location.href) => {
+  name = name.replace(/[[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+};
+
 AesirAnalytics();
--- src/utils.js
+++ src/utils.js
@@ -1,21 +1,8 @@
 import axios from 'axios';
+import { trackerService } from './services';
 
-let cache;
-const assign = (a, b) => {
-  Object.keys(b).forEach((key) => {
-    if (b[key] !== undefined) a[key] = b[key];
-  });
-  return a;
-};
-
-const endpointInit = (root) => {
-  return `${root}/index.php?webserviceClient=site&webserviceVersion=1.0.0&option=reditem&view=webevent&task=init&api=hal`;
-};
-const endpointStart = (root) => {
-  return `${root}/index.php?webserviceClient=site&webserviceVersion=1.0.0&option=reditem&view=webevent&task=start&api=hal`;
-};
-const endpointEnd = (root) => {
-  return `${root}/index.php?webserviceClient=site&webserviceVersion=1.0.0&option=reditem&view=webevent&task=end&api=hal`;
+const createRequest = (endpoint, task) => {
+  return `${endpoint}/index.php?webserviceClient=site&webserviceVersion=1.0.0&option=reditem&view=webevent&task=${task}&api=hal`;
 };
 const getIpAddress = async () => {
   const res = await axios.get('https://geolocation-db.com/json/');
@@ -34,7 +21,7 @@ const initTracker = async (endpoint, url, referrer, user_agent) => {
   const urlParams = new URLSearchParams(queryString);
   if (!urlParams.get('event_id') && !urlParams.get('uuid')) {
     let ip = await getIpAddress();
-    const response = await trackerService(endpointInit(endpoint), {
+    const response = await trackerService(createRequest(endpoint, 'init'), {
       url: url,
       referrer: referrer,
       user_agent: user_agent,
@@ -51,7 +38,7 @@ const startTracker = async (endpoint, event_id, uuid, referrer) => {
   const url = location.protocol + '//' + location.host + location.pathname;
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const responseStart = await trackerService(endpointStart(endpoint), {
+  const responseStart = await trackerService(createRequest(endpoint, 'start'), {
     ...(urlParams.get('event_id') && {
       event_id: urlParams.get('event_id'),
     }),
@@ -75,7 +62,7 @@ const endTracker = async (endpoint, event_id, uuid) => {
   // End Tracker
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const responseEnd = await trackerService(endpointEnd(endpoint), {
+  const responseEnd = await trackerService(createRequest(endpoint, 'end'), {
     ...(urlParams.get('event_id_start') && {
       event_id: urlParams.get('event_id_start'),
     }),
@@ -92,125 +79,4 @@ const endTracker = async (endpoint, event_id, uuid) => {
   return responseEnd;
 };
 
-/* SERVICES */
-const trackerService = async (endpoint, payload) => {
-  const fetchData = await fetch(endpoint, {
-    method: 'POST',
-    body: JSON.stringify(payload),
-    headers: assign({ 'Content-Type': 'application/json' }, { ['x-tracker-cache']: cache }),
-  });
-  const response = await fetchData.json();
-  return response;
-};
-
-const AesirAnalytics = () => {
-  const hook = (_this, method, callback) => {
-    const orig = _this[method];
-
-    return (...args) => {
-      callback.apply(null, args);
-
-      return orig.apply(_this, args);
-    };
-  };
-
-  /* Handle history changes */
-
-  const handlePush = async (state, title, url) => {
-    if (!url) return;
-    const { pathname, search, origin } = location;
-    url = `${origin}${pathname}${search}`;
-    const newUrl = url.toString();
-
-    if (newUrl.substring(0, 4) === 'http') {
-      url = '/' + newUrl.split('/').splice(3).join('/');
-    } else {
-      url = newUrl;
-    }
-
-    // if (currentUrl !== currentRef) {
-    //   await initTracker();
-    //   startTracker();
-    // }
-  };
-
-  /* Global */
-
-  if (!window.tracker) {
-    const tracker = (eventValue) => eventValue;
-    tracker.initTracker = initTracker;
-    tracker.startTracker = startTracker;
-
-    window.tracker = tracker;
-  }
-
-  /* Start */
-
-  history.pushState = hook(history, 'pushState', handlePush);
-  history.replaceState = hook(history, 'replaceState', handlePush);
-
-  const envParams = process.env;
-  const hostUrl = envParams.ENDPOINT_ANALYTICS_URL ? envParams.ENDPOINT_ANALYTICS_URL : '';
-
-  const root = hostUrl ? hostUrl.replace(/\/$/, '') : '';
-
-  const update = async () => {
-    if (document.readyState === 'complete') {
-      const responseInit = await initTracker(root);
-      if (responseInit) {
-        responseInit.result.event_id && insertParam('event_id', responseInit.result.event_id);
-        insertParam('uuid', responseInit.result.uuid);
-        replaceUrl();
-      }
-      const responseStart = await startTracker(root);
-      if (responseStart) {
-        responseStart.result.event_id &&
-          insertParam('event_id_start', responseStart.result.event_id);
-        insertParam('uuid_start', responseStart.result.uuid);
-        replaceUrl();
-      }
-    }
-  };
-
-  document.addEventListener('readystatechange', update, true);
-
-  window.addEventListener('beforeunload', async () => {
-    await endTracker(root);
-  });
-
-  update();
-};
-const insertParam = (key, value) => {
-  const url = new URL(window.location.href);
-  url.searchParams.set(key, value);
-  window.history.pushState({ path: url.href }, '', url.href);
-};
-
-const replaceUrl = () => {
-  const urlParams = new URLSearchParams(window.location.search);
-  const event_id = urlParams.get('event_id');
-  const uuid = urlParams.get('uuid');
-
-  let anchors = document.getElementsByTagName('a');
-  for (let i = 0; i < anchors.length; i++) {
-    const eventIdParams = getParameterByName('event_id', anchors[i].href);
-    const uuidParams = getParameterByName('uuid', anchors[i].href);
-    if (anchors[i].href) {
-      const url = new URL(anchors[i].href);
-      !eventIdParams && event_id && url.searchParams.append('event_id', event_id);
-      !uuidParams && uuid && url.searchParams.append('uuid', uuid);
-      anchors[i].href = url.href;
-    }
-  }
-};
-
-const getParameterByName = (name, url = window.location.href) => {
-  name = name.replace(/[[\]]/g, '\\$&');
-  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-    results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return '';
-  return decodeURIComponent(results[2].replace(/\+/g, ' '));
-};
-
-export { initTracker, startTracker, endTracker, replaceUrl, AesirAnalytics };
+export { initTracker, startTracker, endTracker };
