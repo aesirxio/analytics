c145ee6c49a2b6063f8235a0cc8c2f8924d95cce
Viet Nguyen <viet.nguyen@redweb.dk>
2022-11-25 10:06:46 +0700

    Fix Regex

--- src/analytics.js
+++ src/analytics.js
@@ -100,12 +100,24 @@ const replaceUrl = () => {
 };
 
 const getParameterByName = (name, url = window.location.href) => {
-  name = name.replace(/[[\]]/g, '\\$&');
-  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-    results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return '';
-  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+  if (url && validURL(url)) {
+    let params = new URL(url).searchParams;
+    return params.get(name);
+  }
+  return;
+};
+
+const validURL = (str) => {
+  var pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  ); // fragment locator
+  return !!pattern.test(str);
 };
 
 AesirAnalytics();
