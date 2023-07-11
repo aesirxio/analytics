028c8ccb8fcffea8ee15ae3064de96c17e46310b
Viet <viet@r-digital.tech>
2023-07-11 15:59:43 +0700

    Update

--- src/AnalyticsNext/index.tsx
+++ src/AnalyticsNext/index.tsx
@@ -3,18 +3,12 @@ import React, { ReactNode } from 'react';
 import { AnalyticsContextProvider } from '../utils/AnalyticsContextProvider';
 import AnalyticsHandle from './handle';
 
-interface AnalyticsReact {
-  router: {
-    asPath: string;
-    events: { on: (_: string, __: () => void) => void; off: (_: string, __: () => void) => void };
-    replace: any;
-    query: any;
-    push: any;
-  };
+interface AnalyticsNext {
+  router: any;
   children?: ReactNode;
 }
 
-const AnalyticsNext = ({ router, children }: AnalyticsReact) => {
+const AnalyticsNext = ({ router, children }: AnalyticsNext) => {
   return (
     <>
       <AnalyticsContextProvider>
