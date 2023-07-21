ff99093fd394bbc3be72165d17f0ead9d2abc4a7
Lu Nguyen <lu@r-digital.tech>
2023-07-21 14:14:59 +0700

    Preparation for release v.2.0.0

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'tsup';
 import { sassPlugin } from 'esbuild-sass-plugin';
-import { ScssModulesPlugin } from 'esbuild-scss-modules-plugin';
 import inlineImage from 'esbuild-plugin-inline-image';
-
-const env = process.env.NODE_ENV;
 
 export default defineConfig([
   {
@@ -15,11 +12,7 @@ export default defineConfig([
     loader: {
       '.js': 'jsx',
     },
-    esbuildPlugins: [
-      inlineImage({ limit: -1 }),
-      ScssModulesPlugin({ localsConvention: 'dashes' }),
-      sassPlugin({ type: 'style' }),
-    ],
+    esbuildPlugins: [inlineImage({ limit: -1 }), sassPlugin({ type: 'style' })],
     esbuildOptions(options) {
       options.drop = ['console'];
     },
@@ -31,14 +24,10 @@ export default defineConfig([
   },
   {
     entry: ['src/analytics.tsx'],
-    minify: env === 'production',
+    minify: true,
     format: ['iife'],
     platform: 'browser',
-    esbuildPlugins: [
-      inlineImage({ limit: -1 }),
-      ScssModulesPlugin({ localsConvention: 'dashes' }),
-      sassPlugin({ type: 'style' }),
-    ],
+    esbuildPlugins: [inlineImage({ limit: -1 }), sassPlugin({ type: 'style' })],
     terserOptions: {
       compress: { drop_console: true },
     },
