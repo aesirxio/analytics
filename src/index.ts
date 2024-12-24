61496a2e9cbcc4d20cda2a0e604821cef4802585
Viet <viet@r-digital.tech>
2024-12-24 14:39:11 +0700

    Update export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { default as AnalyticsNext } from './AnalyticsNext';
 export { default as AnalyticsReact } from './AnalyticsReact';
-export { trackEvent } from './utils';
+export { trackEvent, startTracker, endTrackerVisibilityState } from './utils';
 export { AnalyticsContext } from './utils/AnalyticsContextProvider';
 export { default as getFingerprint } from './lib/fingerprint';
 export { default as OptInConsent } from './Components/OptInConsent';
